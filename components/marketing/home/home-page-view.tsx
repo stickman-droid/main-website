3f5c2c92c3dba368e8d081c8dfc2732e357d751d
hemanth18 <hg510443@gmail.com>
2026-04-02 02:47:41 +0530

    compose home page sections

---
+++ components/marketing/home/home-page-view.tsx
@@ -0,0 +1,17 @@
+import { Hero } from "./hero";
+import { GapSection } from "./gap-section";
+import { OfferSection } from "./offer-section";
+import { WorkSection } from "./work-section";
+import { ContactSection } from "./contact-section";
+
+export function HomePageView() {
+  return (
+    <div className="flex flex-col">
+      <Hero />
+      <GapSection />
+      <OfferSection />
+      <WorkSection />
+      <ContactSection />
+    </div>
+  );
+}
