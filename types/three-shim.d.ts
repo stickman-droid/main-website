1133a4f0c645f22233576b7976fe6e42de723595
hemanth18 <hg510443@gmail.com>
2026-04-04 03:09:37 +0530

    Add local three module type shim

---
+++ types/three-shim.d.ts
@@ -0,0 +1,11 @@
+declare module "three" {
+  export * from "../node_modules/.pnpm/@types+three@0.183.1/node_modules/@types/three/index";
+}
+
+declare module "three/examples/jsm/loaders/MTLLoader.js" {
+  export * from "../node_modules/.pnpm/@types+three@0.183.1/node_modules/@types/three/examples/jsm/loaders/MTLLoader";
+}
+
+declare module "three/examples/jsm/loaders/OBJLoader.js" {
+  export * from "../node_modules/.pnpm/@types+three@0.183.1/node_modules/@types/three/examples/jsm/loaders/OBJLoader";
+}
