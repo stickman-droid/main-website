d4a58b24ba082a1dc5494bccf65b6a44eb606e3c
hemanth18 <hg510443@gmail.com>
2026-08-16 01:04:47 +0530

    legal: update terms of service page view

--- components/marketing/legal/terms-page-view.tsx
+++ components/marketing/legal/terms-page-view.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export function TermsPageView() {
   return (
     <div className="min-h-screen bg-background text-[#252525]">
-      <section className="w-full py-12 lg:py-24">
+      <section data-analytics-section="terms_content" className="w-full py-12 lg:py-24">
         <div className="mx-auto w-full max-w-4xl px-6 lg:px-12">
           {/* Breadcrumb */}
           <nav
